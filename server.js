db092f3e7b08cc04d1457b42aa67b9c71bc62fd9
joe kim <j80n89@gmail.com>
2019-01-07 16:49:51 -0500

    route index.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const router = require('./routes/index')
+const methodOverride = require('method-override')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'))
+
+app.set("view engine", "hbs")
 
 const PORT = process.env.PORT || 3000
 
