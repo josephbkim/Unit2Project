e6ec4766e961184e834350be4cc4028eea69b386
joe kim <j80n89@gmail.com>
2019-01-08 11:55:21 -0500

    ok ok

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const
+const appController = require('../controllers/appController')
 const userController = require('../controllers/userController')
 
-router.get('/', userController.index)
-// router.post('/', userController.create)
-// router.get('/new', userController.new)
-// router.get('/:id', userController.show)
-// router.get('/:id/edit', userController.edit)
-// router.patch('/:id', userController.update)
-// router.delete('/:id', userController.delete)
+
+router.get('/user', userController.index)
+// router.post('/user', userController.create)
+// router.get('/user/new', userController.new)
+// router.get('/user/:id', userController.show)
+// router.get('/user/:id/edit', userController.edit)
+// router.patch('/user/:id', userController.update)
+// router.delete('/user/:id', userController.delete)
 
 module.exports = router
