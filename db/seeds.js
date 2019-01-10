8b3d9358b2a8339bae6319e485f516cdbbd0c4db
joe kim <j80n89@gmail.com>
2019-01-10 09:23:09 -0500

    change seed setup

--- db/seeds.js
+++ db/seeds.js
@@ -3,55 +3,55 @@ const Tool = require('../models/Tool')
 const User = require('../models/User')
 
 
-const newNote1 = new Note({
-    createdAt: "Now",
-    noteContent: "Broken"
-})
+// const newNote1 = new Note({
+//     createdAt: "Now",
+//     noteContent: "Broken"
+// })
 
-const newNote2 = new Note({
-    CreatedAt: "Now",
-    noteContent: "Cool"
+// const newNote2 = new Note({
+//     CreatedAt: "Now",
+//     noteContent: "Cool"
 
-})
+// })
 
-const newTool1 = new Tool({
-    toolName: "Cutoff Saw",
-    toolCo: "Stihl",
-    toolModel: "TS420",
-    checkOut: "date.now",
-    note: [newNote1, newNote2]
-})
+// const newTool1 = new Tool({
+//     toolName: "Cutoff Saw",
+//     toolCo: "Stihl",
+//     toolModel: "TS420",
+//     checkOut: "date.now",
+// employTag: [employ1],
+//     note: [newNote1]
+// })
 
-const newTool2 = new Tool({
-    toolName: "Hammer Drill",
-    toolCo: "Bosch",
-    toolModel: "TModel#  HD18-2",
-    checkOut: "date.now",
-    note: [newNote1, newNote2]
-})
+// const newTool2 = new Tool({
+//     toolName: "Hammer Drill",
+//     toolCo: "Bosch",
+//     toolModel: "TModel#  HD18-2",
+//     checkOut: "date.now",
+// employTag: [employ2],
+//     note: [newNote2]
+// })
 
-const newUser1 = new User({
-    name: "Bob the Builder",
-    department: "Residential",
-    tools: [newTool1, newTool2]
-})
+// const employ1 = new User({
+//     name: "Bob the Builder",
+//     department: "Residential",
+// })
 
-const newUser2 = new User({
-    name: "Paul Bunyun",
-    department: "Lumber",
-    tools: [newTool1, newTool2]
-})
+// const employ2 = new User({
+//     name: "Paul Bunyun",
+//     department: "Lumber",
+// })
 
-User.remove({})
-    .then(() => Note.remove({}))
-    .then(() => Tool.remove({}))
-    .then(() => Tool.insertMany([newTool1, newTool2]))
-    .then(() => Note.insertMany([newNote1, newNote2]))
-    .then(() => newUser1.save())
-    .then(() => newUser2.save())
-    .then(() => {
-        console.log("Seeds file finished loading")
-    })
+// User.remove({})
+//     .then(() => Note.remove({}))
+//     .then(() => User.remove({}))
+//     .then(() => User.insertMany([employ1, employ2]))
+//     .then(() => Note.insertMany([newNote1, newNote2]))
+//     .then(() => newTool1.save())
+//     .then(() => newTool2.save())
+//     .then(() => {
+//         console.log("Seeds file finished loading")
+//     })
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/userController')
+// const userController = require('../controllers/userController')
 const toolController = require('../controllers/toolController')
 
-router.get('/', userController.index)
-router.post('/', userController.create)
-router.get('/new', userController.new)
-router.get('/:id', userController.show)
-router.get('/:id/edit', userController.edit)
-router.patch('/:id', userController.update)
-router.delete('/:id', userController.delete)
+// router.get('/', userController.index)
+// router.post('/', userController.create)
+// router.get('/new', userController.new)
+// router.get('/:id', userController.show)
+// router.get('/:id/edit', userController.edit)
+// router.patch('/:id', userController.update)
+// router.delete('/:id', userController.delete)
 
 
-router.get('/:id/tools', toolController.index)
-router.post('/:id/tools', toolController.create)
-router.get('/:id/tools/new', toolController.new)
-// // router.get('tools/:id', toolController)
+// router.get('/:id/tools', toolController.index)
+// router.post('/:id/tools', toolController.create)
+// router.get('/:id/tools/new', toolController.new)
+// // // router.get('tools/:id', toolController)
 
 module.exports = router
 
