bcf25e36ff49a7a6a836fa30544af736ff95a1e0
joe kim <j80n89@gmail.com>
2019-01-10 12:05:40 -0500

    model 2 routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// const userController = require('../controllers/userController')
+const userController = require('../controllers/userController')
 const toolController = require('../controllers/toolController')
 
 router.get('/', toolController.index)
@@ -10,6 +10,15 @@ router.get('/:id/show', toolController.show)
 router.get('/:id/edit', toolController.edit)
 router.patch('/:id', toolController.update)
 router.delete('/:id', toolController.delete)
+
+
+router.get('/:id/users', userController)
+router.post('/:id/users', userController.create)
+router.get('/:id/users/new', toolController.show)
+router.get('/:id/users/:id/show', userController)
+router.get('/:id/users/:id/edit', userController)
+router.patch('/:id/users/:id', userController.update)
+router.delete('/:id/users/:id', userController)
 
 
 
