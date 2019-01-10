7a72174edeec8cbeb91f0682a1359ce26b618404
joe kim <j80n89@gmail.com>
2019-01-10 10:11:59 -0500

    routes and controllers for tools

--- controllers/toolController.js
+++ controllers/toolController.js
@@ -7,11 +7,55 @@ const toolController = {
             console.log(invTools)
             res.render('toolss/index', { invTools })
         })
-    }
-}
+    },
 
-const toolController = {
-    new: res.render
+    new: (req, res) => {
+        res.render("toolss/new")
+    },
+
+    create: (req, res) => {
+        console.log(req.body)
+        Tool.create({
+            toolName: req.body.toolName,
+            toolModel: req.body.toolModel,
+            checkOut: req.body.checkOut,
+            employTag: req.body.employTag,
+            notes: req.body.notes
+        }).then(newTools => {
+            res.redirect('/')
+        })
+    },
+
+    show: (req, res) => {
+        const newToolId = req.params.index
+        Tool.findById(newToolId).then((newTool) => {
+            console.log(newTool)
+            res.render('toolss/show')
+        })
+    },
+
+    edit: (req, res) => {
+        const newToolId = req.params.index
+        res.render('toolss/edit', { newToolId })
+    },
+
+    update: (req, res) => {
+        const newToolId = req.params.index
+        console.log(req.body)
+        Tool.findByIdAndUpdate(newToolId, req.body, {
+            new: true
+        }).then((newTool) => {
+            res.redirect(`/$newToolId`)
+        })
+    },
+
+    delete: (req, res) => {
+        const newToolId = req.params.index
+        Tool.findByIdAndRemove(newToolId).then(() => {
+            res.redirect('/')
+        })
+    }
+
 }
 
 module.exports = toolController
