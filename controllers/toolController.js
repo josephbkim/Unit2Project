dbbfc3f3f74a856a4c2c69145249546ff6dab3e1
joe kim <j80n89@gmail.com>
2019-01-10 09:17:00 -0500

    3 reset

--- controllers/toolController.js
+++ controllers/toolController.js
@@ -1,32 +1,32 @@
 const User = require('../models/User')
 const Tool = require("../models/Tool")
 
-const toolController = {
-    index: (req, res) => {
-        const newToolId = req.params.id
-        console.log(newToolId)
-        Tool.findById(newToolId).populate('tools').then((newTool) => {
-            const bananaTools = newTool.tools
-            res.render('toolss/index', { bananaTools, newToolId })
-        })
-    },
+// const toolController = {
+//     index: (req, res) => {
+//         const newToolId = req.params.id
+//         console.log(newToolId)
+//         Tool.findById(newToolId).populate('tools').then((newTool) => {
+//             const bananaTools = newTool.tools
+//             res.render('toolss/index', { bananaTools, newToolId })
+//         })
+//     },
 
-    new: (req, res) => {
-        res.render("toolss/new")
-    },
+//     new: (req, res) => {
+//         res.render("toolss/new")
+//     },
 
-    create: (req, res) => {
-        console.log(req.body)
-        User.create({
-            toolName: req.body.toolName,
-            toolCo: req.body.toolCo,
-            toolModel: req.body.toolModel,
-            checkOut: req.body.checkOut,
-            note: req.body.note
-        }).then(newTool => {
-            res.redirect('/')
-        })
-    }
-}
+//     create: (req, res) => {
+//         console.log(req.body)
+//         User.create({
+//             toolName: req.body.toolName,
+//             toolCo: req.body.toolCo,
+//             toolModel: req.body.toolModel,
+//             checkOut: req.body.checkOut,
+//             note: req.body.note
+//         }).then(newTool => {
+//             res.redirect('/')
+//         })
+//     }
+// }
 
 module.exports = toolController
--- models/Tool.js
+++ models/Tool.js
@@ -6,6 +6,10 @@ const Tool = new Schema({
     toolCo: String,
     toolModel: String,
     checkOut: String,
+    employTag: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
     notes: [{
         type: Schema.Types.ObjectId,
         ref: 'Note'
